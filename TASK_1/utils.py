3fb77327f4e830571919e4cc9e9eb272cffe53fd
francealip <f.aliprandi@studenti.unipi.it>
2024-10-24 12:44:37 +0200

    added first feature engineering

--- TASK_1/utils.py
+++ TASK_1/utils.py
@@ -2,26 +2,27 @@
 from typing import Tuple, Dict, Any
 
 from sklearn.preprocessing import StandardScaler
-import pandas 
+import pandas as pd 
 import matplotlib.pyplot as plt
 import seaborn as sns
+import numpy as np
 
 
 # DATA PREPROCESSING FUNCTIONS 
 
 #correlation function 
-def correlations(dataset: pandas.DataFrame) -> pandas.DataFrame:
+def correlations(dataset: pd.DataFrame) -> pd.DataFrame:
     correlations_dictionary = {
         correlation_type: dataset.corr(numeric_only=True, method=correlation_type)
         for correlation_type in ("kendall", "pearson", "spearman")
     }
     for i, k in enumerate(correlations_dictionary.keys()):
         correlations_dictionary[k].loc[:, "correlation_type"] = k
-    correlations_matrix = pandas.concat(correlations_dictionary.values())
+    correlations_matrix = pd.concat(correlations_dictionary.values())
 
     return correlations_matrix
 
-def plot_correlations(correlations: pandas.DataFrame, fig_size: tuple = (18,6)) -> None:
+def plot_correlations(correlations: pd.DataFrame, fig_size: tuple = (18,6)) -> None:
     # Plot heatmaps for each correlation type
     correlation_types = correlations['correlation_type'].unique()
 
@@ -37,8 +38,8 @@ def plot_correlations(correlations: pandas.DataFrame, fig_size: tuple = (18,6)) 
     plt.show()
 
 # single feature transformation function
-def __transform_single_features(dataset: pandas.DataFrame, transformation: str) -> Tuple[
-    pandas.DataFrame, Dict[str, Any]]:
+def __transform_single_features(dataset: pd.DataFrame, transformation: str) -> Tuple[
+    pd.DataFrame, Dict[str, Any]]:
     match transformation:
         case "standard":
             transformed_dataset = dataset.copy().select_dtypes(exclude=["object", "category", "bool", "datetime64"])
@@ -55,12 +56,12 @@ def __transform_single_features(dataset: pandas.DataFrame, transformation: str) 
     return transformed_dataset, transformations
 
 # center and scale function
-def center_and_scale(dataset: pandas.DataFrame) -> Tuple[pandas.DataFrame, Dict[str, Any]]:
+def center_and_scale(dataset: pd.DataFrame) -> Tuple[pd.DataFrame, Dict[str, Any]]:
     """Shifts data to the origin: removes mean and scales by standard deviation all numeric features. Returns a copy of the dataset."""
     return __transform_single_features(dataset, "standard")
 
 # outlier counts function
-def count_outliers(column: pandas.DataFrame) -> pandas.Series:
+def count_outliers(column: pd.DataFrame) -> pd.Series:
     """Counts the number of outliers in each numeric feature using the IQR method. Returns the counts."""
     # Printing number of outliers basing on the IQR method
     Q1 = column.quantile(0.25)
@@ -70,3 +71,11 @@ def count_outliers(column: pandas.DataFrame) -> pandas.Series:
 
     print(f"Number of outliers: {len(outliers)} over {len(column) - column.isnull().sum()} values")
     return None
+
+# BMI index function
+
+def compute_bmi(weight, height):
+    if pd.notnull(weight) and pd.notnull(height):
+        return round(weight / ((height / 100) ** 2), 2)
+    else:
+        return np.nan
